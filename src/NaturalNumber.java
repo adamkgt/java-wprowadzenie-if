68e6adde3d2e33e43ee9f5edc7ab0661a6e5eafb
Adam Kogut <adamkgt@gmail.com>
2020-12-20 19:27:47 +0100

    #9 Implementacja Zadanie4. Przechowywanie oraz sprawdzanie liczb naturalnych.

---
+++ src/NaturalNumber.java
@@ -0,0 +1,17 @@
+public class NaturalNumber {
+    public static void main(String[] args) {
+
+         int[] Numbers= new int[5];
+        Numbers[0] = 1;
+        Numbers[1] = 2;
+        Numbers[2] = 3;
+        Numbers[3] = 4;
+        Numbers[4] = 5;
+
+        if (Numbers[0]*2+4<8) {
+            System.out.println("Tak");
+        }else if (Numbers[0]*2+4>=8)
+             System.out.println("Nie");
+
+    }
+}
